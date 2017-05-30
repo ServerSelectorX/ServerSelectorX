2250d069359dc62d6f78868927c535a189a9ebbe
RobinMC <robinslot@hotmail.nl>
2017-05-30 15:49:03 +0200

    Added sound functionality (play sound when player right clicks on selector menu)

--- src/com/robinmc/serverselectorx/SelectorOpenListener.java
+++ src/com/robinmc/serverselectorx/SelectorOpenListener.java
@@ -1,6 +1,9 @@
 package com.robinmc.serverselectorx;
 
+import java.util.logging.Level;
+
 import org.bukkit.Material;
+import org.bukkit.Sound;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
@@ -39,6 +42,18 @@ public class SelectorOpenListener implements Listener {
 					return;
 				}
 			}
+			
+			//Play sound
+			String soundString = Config.getConfig().getString("selector-open-sound");
+			if (soundString != null && !soundString.equals("NONE")){
+				try {
+					Sound sound = Sound.valueOf(soundString);
+					player.playSound(player.getLocation(), sound, 1.0f, 1.0f);
+				} catch (IllegalArgumentException e){
+					Main.getPlugin().getLogger().log(Level.WARNING, "A sound with the name " + soundString + " could not be found. Make sure that it is the right name for your server version.");
+				}
+			}
+			
 			SelectorMenu.open(player);
 		}
 	}
