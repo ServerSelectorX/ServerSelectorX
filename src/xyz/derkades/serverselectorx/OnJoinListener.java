7047962e3aa82ab0468e7ad66c721b294a9ec1ef
RobinMC <robinslot@hotmail.nl>
2017-06-14 20:56:29 +0200

    Fix item on join

--- src/xyz/derkades/serverselectorx/OnJoinListener.java
+++ src/xyz/derkades/serverselectorx/OnJoinListener.java
@@ -1,7 +1,9 @@
 package xyz.derkades.serverselectorx;
 
+import java.io.File;
+
 import org.bukkit.Material;
-import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.configuration.file.YamlConfiguration;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
@@ -17,8 +19,31 @@ public class OnJoinListener implements Listener {
 	
 	@EventHandler(priority = EventPriority.MONITOR)
 	public void onJoin(PlayerJoinEvent event){
-		FileConfiguration config = Main.getPlugin().getConfig();
-		
+		for (File serverSelectorFile : new File(Main.getPlugin().getDataFolder() + "/menu").listFiles()){
+			YamlConfiguration config = YamlConfiguration.loadConfiguration(serverSelectorFile);
+			
+			boolean putItemInInventory = config.getBoolean("on-join");
+			if (!putItemInInventory)
+				continue;
+			
+			Player player = event.getPlayer();
+			Material material = Material.getMaterial(config.getString("item"));
+			
+			if (material == null){
+				player.sendMessage(Message.INVALID_ITEM_NAME.toString());
+				return;
+			}
+			
+			ItemStack item = new ItemBuilder(material)
+					.setName(Colors.parseColors(config.getString("title")))
+					.create();
+			
+			int slot = config.getInt("inv-slot");
+			PlayerInventory inv = player.getInventory();
+			
+			inv.setItem(slot, item);
+		}
+		/*
 		boolean putItemInInventory = config.getBoolean("on-join");
 		if (!putItemInInventory)
 			return;
@@ -38,7 +63,7 @@ public class OnJoinListener implements Listener {
 		int slot = config.getInt("inv-slot");
 		PlayerInventory inv = player.getInventory();
 		
-		inv.setItem(slot, item);
+		inv.setItem(slot, item);*/
 	}
 
 }
