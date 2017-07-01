c47a8a2c9907c30e32815b9e7cba793f1717f693
RobinMC <robinslot@hotmail.nl>
2017-07-01 11:50:36 +0200

    Added cancel move/drop feature

---
+++ src/xyz/derkades/serverselectorx/ItemMoveDropCancelListener.java
@@ -0,0 +1,30 @@
+package xyz.derkades.serverselectorx;
+
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
+import org.bukkit.event.Listener;
+import org.bukkit.event.inventory.InventoryClickEvent;
+import org.bukkit.event.inventory.InventoryDragEvent;
+import org.bukkit.event.player.PlayerDropItemEvent;
+
+public class ItemMoveDropCancelListener implements Listener {
+	
+	public static boolean DROP_PERMISSION_ENABLED = false;
+	public static boolean MOVE_PERMISSION_ENABLED = false;
+	
+	@EventHandler(ignoreCancelled = true, priority = EventPriority.LOW)
+	public void onDrop(PlayerDropItemEvent event){
+		event.setCancelled(DROP_PERMISSION_ENABLED && !event.getPlayer().hasPermission("ssx.drop"));
+	}
+	
+	@EventHandler(ignoreCancelled = true, priority = EventPriority.LOW)
+	public void onItemMove(InventoryClickEvent event){
+		event.setCancelled(MOVE_PERMISSION_ENABLED && !event.getWhoClicked().hasPermission("ssx.move"));
+	}
+	
+	@EventHandler(ignoreCancelled = true, priority = EventPriority.LOW)
+	public void onItemMove(InventoryDragEvent event){
+		event.setCancelled(MOVE_PERMISSION_ENABLED && !event.getWhoClicked().hasPermission("ssx.move"));
+	}
+
+}
