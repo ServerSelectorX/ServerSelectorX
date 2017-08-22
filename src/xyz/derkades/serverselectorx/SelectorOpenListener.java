0e3ab6a0fdecd983d4c71615299d1e2ac3b8af3e
Robin <robinslot@hotmail.nl>
2017-08-22 13:21:48 +0200

    Remove debug messages

--- src/xyz/derkades/serverselectorx/SelectorOpenListener.java
+++ src/xyz/derkades/serverselectorx/SelectorOpenListener.java
@@ -22,15 +22,10 @@ public class SelectorOpenListener implements Listener {
 		
 		Player player = event.getPlayer();
 		
-		System.out.println("[ssx debug] Player clicked");
-		
 		//Small cooldown because on 1.9+ interact event is called twice.
 		if (Cooldown.getCooldown(player.getUniqueId() + "doubleopen") > 0) {
-			System.out.println("[ssx debug] On cooldown, " + Cooldown.getCooldown(player.getUniqueId() + "doubleopen") + " milliseconds left.");
 			return;
 		}
-		
-		System.out.println("[ssx debug] not on cooldown");
 		
 		Cooldown.addCooldown(player.getUniqueId() + "doubleopen", 200); //Add cooldown for 0.2 seconds
 		
