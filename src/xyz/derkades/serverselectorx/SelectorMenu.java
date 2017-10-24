d09973f10186e5b4e8fb2431855472a368b955fc
Derkades <robinslot@hotmail.nl>
2017-10-24 17:30:32 +0200

    Debug messages

--- src/xyz/derkades/serverselectorx/SelectorMenu.java
+++ src/xyz/derkades/serverselectorx/SelectorMenu.java
@@ -61,9 +61,12 @@ public class SelectorMenu extends IconMenu {
 						if (Main.LAST_INFO_TIME.containsKey(serverName)) {
 							// If the server has not sent a message for 2 seconds (usually the server sends a message every second)
 							online = Main.LAST_INFO_TIME.get(serverName) + 2000 < System.currentTimeMillis();
+							System.out.println("[debug] LAST_INFO_TIME containsKey " + Main.LAST_INFO_TIME.get(serverName));
+							System.out.println("[debug] current time " + System.currentTimeMillis());
 						} else {
 							//If the server has not sent a message at all it is offline
 							online = false;
+							System.out.println("[debug] no message received");
 						}
 						
 						if (online) {
