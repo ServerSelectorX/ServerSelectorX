082e26e8ed6553874a090d1767b702f77ae792e1
Derkades <robinslot@hotmail.nl>
2017-10-24 15:52:45 +0200

    Write placeholder receiver

--- src/xyz/derkades/serverselectorx/PlaceholderReceiver.java
+++ src/xyz/derkades/serverselectorx/PlaceholderReceiver.java
@@ -1,5 +1,9 @@
 package xyz.derkades.serverselectorx;
 
+import java.util.HashMap;
+import java.util.Map;
+
+import com.google.gson.Gson;
 import com.mitch528.sockets.events.MessageReceivedEvent;
 import com.mitch528.sockets.events.MessageReceivedEventListener;
 
@@ -8,6 +12,15 @@ public class PlaceholderReceiver /*implements PluginMessageListener*/ implements
 	@Override
 	public void messageReceived(MessageReceivedEvent event) {
 		Main.getPlugin().getLogger().info("Recieved message: " + event.getMessage());
+		Gson gson = new Gson();
+		
+		Map<String, String> genericSample = new HashMap<>();
+		
+		@SuppressWarnings("unchecked")
+		Map<String, String> placeholders = gson.fromJson(event.getMessage(), genericSample.getClass());
+		
+		String server = placeholders.remove("server");
+		Main.PLACEHOLDERS.put(server, placeholders);
 	}
 	
 	/*@Override
