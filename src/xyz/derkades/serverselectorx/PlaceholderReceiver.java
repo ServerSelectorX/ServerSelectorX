1b50e3eb418dc82d9e18c1e09ef36ac7b062b10b
Robin <robinslot@hotmail.nl>
2017-12-25 21:48:37 +0100

    remove unused import

--- src/xyz/derkades/serverselectorx/PlaceholderReceiver.java
+++ src/xyz/derkades/serverselectorx/PlaceholderReceiver.java
@@ -10,8 +10,6 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import org.apache.commons.codec.digest.DigestUtils;
-
 import com.google.gson.Gson;
 
 public class PlaceholderReceiver /*implements MessageReceivedEventListener*/ extends HttpServlet {
