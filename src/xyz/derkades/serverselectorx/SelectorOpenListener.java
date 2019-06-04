a18d8b4544b9984930cede3341a7ee45607bd3ce
Robin <robinslot@hotmail.nl>
2019-06-05 00:43:03 +0200

    Replace cooldown with hand check

--- src/xyz/derkades/serverselectorx/SelectorOpenListener.java
+++ src/xyz/derkades/serverselectorx/SelectorOpenListener.java
@@ -9,55 +9,51 @@ import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.Action;
 import org.bukkit.event.player.PlayerInteractEvent;
+import org.bukkit.inventory.EquipmentSlot;
 import org.bukkit.inventory.ItemStack;
-
-import xyz.derkades.derkutils.Cooldown;
 
 public class SelectorOpenListener implements Listener {
 
 	@SuppressWarnings("deprecation")
 	@EventHandler(priority = EventPriority.HIGH)
-	public void onInteract(PlayerInteractEvent event){
+	public void onInteract(final PlayerInteractEvent event){
+		if (event.getHand() == EquipmentSlot.OFF_HAND) {
+			return;
+		}
+
 		final FileConfiguration globalConfig = Main.getConfigurationManager().getGlobalConfig();
-		
+
 		if (event.isCancelled() && globalConfig.getBoolean("ignore-cancelled", false)) {
 			return;
 		}
-		
+
 		final boolean openOnRightClick = globalConfig.getBoolean("right-click-open", true);
 		final boolean openOnLeftClick = globalConfig.getBoolean("left-click-open", false);
-		
+
 		if (!(
 				openOnRightClick && (event.getAction() == Action.RIGHT_CLICK_AIR || event.getAction() == Action.RIGHT_CLICK_BLOCK) ||
 				openOnLeftClick && (event.getAction() == Action.LEFT_CLICK_AIR || event.getAction() == Action.LEFT_CLICK_BLOCK)
 				)) return;
-		
-		Player player = event.getPlayer();
-		
-		//Small cooldown because on 1.9+ interact event is called twice.
-		if (Cooldown.getCooldown(player.getUniqueId() + "doubleopen") > 0) { //if time left on cooldown is > 0
-			return;
-		}
-		
-		for (final Map.Entry<String, FileConfiguration> menuConfigEntry : Main.getConfigurationManager().getAllMenus().entrySet()) {		
+
+		final Player player = event.getPlayer();
+
+		for (final Map.Entry<String, FileConfiguration> menuConfigEntry : Main.getConfigurationManager().getAllMenus().entrySet()) {
 			final String configName = menuConfigEntry.getKey();
 			final FileConfiguration menuConfig = menuConfigEntry.getValue();
-			
+
 			if (!menuConfig.getBoolean("item.enabled")) {
 				continue;
 			}
-			
-			ItemStack item = Main.getHotbarItemStackFromMenuConfig(player, menuConfig, configName);
-			
+
+			final ItemStack item = Main.getHotbarItemStackFromMenuConfig(player, menuConfig, configName);
+
 			if (!item.isSimilar(player.getItemInHand())) {
 				continue;
 			}
-			
-			boolean cancel = globalConfig.getBoolean("cancel-click-event", false);
+
+			final boolean cancel = globalConfig.getBoolean("cancel-click-event", false);
 			if (cancel) event.setCancelled(true);
-			
-			Cooldown.addCooldown(player.getUniqueId() + "doubleopen", 1000); //Add cooldown for 1 second
-			
+
 			Main.openSelector(player, configName);
 			return;
 		}
