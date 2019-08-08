7ad78d90c696c069aebcf66fb75401d413af5a63
Robin <robinslot@hotmail.nl>
2019-08-08 22:38:22 +0200

    Add message action

--- src/xyz/derkades/serverselectorx/actions/MessageAction.java
+++ src/xyz/derkades/serverselectorx/actions/MessageAction.java
@@ -1,5 +1,20 @@
 package xyz.derkades.serverselectorx.actions;
 
+import org.bukkit.entity.Player;
+
+import xyz.derkades.derkutils.bukkit.Colors;
+import xyz.derkades.derkutils.bukkit.PlaceholderUtil;
+
 public class MessageAction extends Action {
+
+	public MessageAction() {
+		super("message", true);
+	}
+
+	@Override
+	public boolean apply(final Player player, final String value) {
+		player.sendMessage(PlaceholderUtil.parsePapiPlaceholders(player, Colors.parseColors(value)));
+		return false;
+	}
 
 }
