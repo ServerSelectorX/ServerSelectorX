f1edd14fa77f31d9af5632ebf3b7c14fc6593fac
Robin <robinslot@hotmail.nl>
2019-10-01 17:06:45 +0200

    Remove unecessary settings from pom.xml

--- src/xyz/derkades/serverselectorx/SelectorOpenListener.java
+++ src/xyz/derkades/serverselectorx/SelectorOpenListener.java
@@ -12,40 +12,41 @@ import org.bukkit.event.player.PlayerInteractEvent;
 import xyz.derkades.derkutils.Cooldown;
 
 public class SelectorOpenListener implements Listener {
-	
+
+	// We need to use the deprecated getItemInHand method to keep compatibility with 1.8
+	@SuppressWarnings("deprecation")
+
 	@EventHandler(priority = EventPriority.HIGH)
 	public void onInteract(final PlayerInteractEvent event){
 		if (!(event.getAction() == Action.RIGHT_CLICK_AIR ||
-				event.getAction() == Action.RIGHT_CLICK_BLOCK)){
+				event.getAction() == Action.RIGHT_CLICK_BLOCK))
 			return;
-		}
-		
+
 		final Player player = event.getPlayer();
-		
+
 		// Small cooldown to prevent weird glitches when right clicking the item very quickly
 		// and because on 1.9+ interact event is called twice.
-		if (Cooldown.getCooldown(player.getUniqueId() + "doubleopen") > 0) {
+		if (Cooldown.getCooldown(player.getUniqueId() + "doubleopen") > 0)
 			return;
-		}
-		
+
 		Cooldown.addCooldown(player.getUniqueId() + "doubleopen", 500);
-		
-		for (final FileConfiguration config : Main.getConfigurationManager().getAll()){	
+
+		for (final FileConfiguration config : Main.getConfigurationManager().getAll()){
 			if (config.getString("item").equalsIgnoreCase("NONE")){
 				continue;
 			}
-			
+
 			final String string = config.getString("item");
 			Material material = Material.getMaterial(string);
-			
+
 			if (material == null){
 				material = Material.STONE;
 			}
-			
+
 			if (player.getInventory().getItemInHand().getType() != material){
 				continue;
 			}
-			
+
 			Main.openSelector(player, config);
 			return;
 		}
