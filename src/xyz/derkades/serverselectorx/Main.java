935510a1455ed3dd44df7aa72f6a886b601f6325
Robin <robinslot@hotmail.nl>
2019-11-03 11:09:21 +0100

    Add data value support

--- src/xyz/derkades/serverselectorx/Main.java
+++ src/xyz/derkades/serverselectorx/Main.java
@@ -57,12 +57,12 @@ public class Main extends JavaPlugin {
 
 		configurationManager = new ConfigurationManager();
 
-		getServer().getMessenger().registerOutgoingPluginChannel(this, "BungeeCord");
+		this.getServer().getMessenger().registerOutgoingPluginChannel(this, "BungeeCord");
 
-		getCommand("serverselectorx").setExecutor(new ServerSelectorXCommand());
+		this.getCommand("serverselectorx").setExecutor(new ServerSelectorXCommand());
 
 		//Register custom selector commands
-		registerCommands();
+		this.registerCommands();
 
 		// Disable annoying jetty warnings
 		if (!configurationManager.getSSXConfig().getBoolean("jetty-debug", false)){
@@ -98,8 +98,8 @@ public class Main extends JavaPlugin {
 
 				// Don't freeze for longer than 5 seconds
 				if (safetyLimit > max) {
-					getLogger().severe("Webserver was still running after waiting for 5 seconds.");
-					getLogger().severe("Giving up, crashing the server is worse than breaking the menu.");
+					this.getLogger().severe("Webserver was still running after waiting for 5 seconds.");
+					this.getLogger().severe("Giving up, crashing the server is worse than breaking the menu.");
 					break;
 				}
 
@@ -142,8 +142,9 @@ public class Main extends JavaPlugin {
 							if (sender instanceof Player){
 								final Player player = (Player) sender;
 								//Small cooldown to prevent weird bugs
-								if (Cooldown.getCooldown(player.getUniqueId() + "doubleopen") > 0)
+								if (Cooldown.getCooldown(player.getUniqueId() + "doubleopen") > 0) {
 									return true;
+								}
 
 								Cooldown.addCooldown(player.getUniqueId() + "doubleopen", 1000); //Add cooldown for 1 second
 
@@ -206,8 +207,9 @@ public class Main extends JavaPlugin {
 	}
 
 	public static void teleportPlayerToServer(final Player player, final String server){
-		if (Cooldown.getCooldown("servertp" + player.getName() + server) > 0)
+		if (Cooldown.getCooldown("servertp" + player.getName() + server) > 0) {
 			return;
+		}
 
 		Cooldown.addCooldown("servertp" + player.getName() + server, 1000);
 
@@ -281,6 +283,9 @@ public class Main extends JavaPlugin {
 		if (section.isInt("amount")) {
 			builder.amount(section.getInt("amount"));
 		}
+
+		final int data = section.getInt("data", 0);
+		builder.damage(data);
 
 		return builder;
     }
