7868c8ea6a8450f61a64d4141d73fc50ed153614
Robin <robinslot@hotmail.nl>
2019-12-04 14:52:20 +0100

    Remove weird aqua color

--- src/xyz/derkades/serverselectorx/ReloadCommand.java
+++ src/xyz/derkades/serverselectorx/ReloadCommand.java
@@ -1,7 +1,5 @@
 package xyz.derkades.serverselectorx;
 
-import static org.bukkit.ChatColor.AQUA;
-
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
@@ -9,24 +7,24 @@ import org.bukkit.command.CommandSender;
 import net.md_5.bungee.api.ChatColor;
 
 public class ReloadCommand implements CommandExecutor {
-	
+
 	@Override
-	public boolean onCommand(CommandSender sender, Command command, String label, String[] args){
+	public boolean onCommand(final CommandSender sender, final Command command, final String label, final String[] args){
 		if (args.length != 1){
 			return false;
 		}
-		
+
 		if (args[0].equalsIgnoreCase("reload") || args[0].equalsIgnoreCase("rl")){
 			if (!sender.hasPermission("ssx.reload")) {
 				sender.sendMessage(ChatColor.RED + "You need the permission 'ssx.reload' to execute this command.");
 				return true;
 			}
-			
+
 			Main.getConfigurationManager().reload();
-			sender.sendMessage(AQUA + "The configuration file has been reloaded.");
+			sender.sendMessage("The configuration file has been reloaded.");
 			return true;
 		}
-		
+
 		return false;
 	}
 
