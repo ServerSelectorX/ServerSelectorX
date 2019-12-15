057ab3fe49fcc5713d02bb894fe1f4fbecf58820
Robin <robinslot@hotmail.nl>
2019-12-15 12:32:33 +0100

    Fixed servers always offline

--- src/xyz/derkades/serverselectorx/SelectorMenu.java
+++ src/xyz/derkades/serverselectorx/SelectorMenu.java
@@ -44,8 +44,8 @@ public class SelectorMenu extends IconMenu {
 				name = section.getString("offline.name", " ");
 				lore = section.getStringList("offline.lore");
 			} else {
-				final String ip = config.getString("ip");
-				final int port = config.getInt("port");
+				final String ip = section.getString("ip");
+				final int port = section.getInt("port");
 				final String serverId = ip + port;
 
 				Map<String, Object> placeholders = null;
