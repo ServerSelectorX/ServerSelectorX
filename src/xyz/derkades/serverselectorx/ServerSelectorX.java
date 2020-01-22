6a7685f0b65855ea8d4dc8ac327381ef0dcb7e96
Robin <robinslot@hotmail.nl>
2020-01-22 11:59:43 +0100

    Add convenience methods to ServerSelectorX class

--- src/xyz/derkades/serverselectorx/ServerSelectorX.java
+++ src/xyz/derkades/serverselectorx/ServerSelectorX.java
@@ -1,11 +1,28 @@
 package xyz.derkades.serverselectorx;
 
+import java.util.List;
+
+import org.bukkit.entity.Player;
+
 import xyz.derkades.serverselectorx.actions.Action;
+import xyz.derkades.serverselectorx.placeholders.Server;
 
 public class ServerSelectorX {
 
 	public static void registerAction(final Action action) {
 		Action.ACTIONS.add(action);
+	}
+	
+	public static boolean runAction(Player player, String actionString) {
+		return Action.runAction(player, actionString);
+	}
+	
+	public static boolean runActions(Player player, List<String> actionStrings) {
+		return Action.runActions(player, actionStrings);
+	}
+	
+	public static Server getServer(String name) {
+		return Server.getServer(name);
 	}
 
 }
