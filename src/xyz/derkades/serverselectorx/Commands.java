ad824404f6508590fad0b2db5bcb8dc1b50885c9
Robin <robinslot@hotmail.nl>
2020-01-22 11:53:17 +0100

    Only put try around the reflection part

--- src/xyz/derkades/serverselectorx/Commands.java
+++ src/xyz/derkades/serverselectorx/Commands.java
@@ -17,51 +17,53 @@ import xyz.derkades.serverselectorx.actions.Action;
 public class Commands {
 
 	static void registerCustomCommands() {
+		final CommandMap commandMap;
 		try {
 			final Field bukkitCommandMap = Bukkit.getServer().getClass().getDeclaredField("commandMap");
-
 			bukkitCommandMap.setAccessible(true);
-			final CommandMap commandMap = (CommandMap) bukkitCommandMap.get(Bukkit.getServer());
-
-			for (final Map.Entry<String, FileConfiguration> configEntry : Main.getConfigurationManager().commands.entrySet()) {
-				final String commandName = configEntry.getKey();
-				final FileConfiguration config = configEntry.getValue();
-				final String description = config.getString("description", "Opens menu");
-				final String usage = config.getString("usage", "/<command>");
-				final List<String> aliases = config.getStringList("aliases");
-				final List<String> actions = config.getStringList("actions");
-
-				if (commandMap.getCommand(commandName) != null) {
-					Main.getPlugin().getLogger().warning("Skipped registering command /" + commandName + ", it already exists.");
-					continue;
-				}
-
-				commandMap.register("ssx-custom", new Command(commandName, description, usage, aliases) {
-
-					@Override
-					public boolean execute(final CommandSender sender, final String label, final String[] args) {
-						if (sender instanceof Player){
-							final Player player = (Player) sender;
-
-							if (config.isInt("cooldown")) {
-								final long timeLeft = Cooldown.getCooldown("ssxcommand" + commandName);
-								if (timeLeft > 0) {
-									player.sendMessage(String.format(Main.getConfigurationManager().misc.getString("cooldown-message"), timeLeft / 1000.0));
-									return true;
-								}
-
-								Cooldown.addCooldown("ssxcommand" + commandName, config.getInt("cooldown"));
-							}
-
-							Action.runActions(player, actions);
-						}
-						return true;
-					}
-				});
-			}
+			commandMap = (CommandMap) bukkitCommandMap.get(Bukkit.getServer());
 		} catch (NoSuchFieldException | IllegalAccessException e) {
 			e.printStackTrace();
+			return;
 		}
+
+		for (final Map.Entry<String, FileConfiguration> configEntry : Main.getConfigurationManager().commands.entrySet()) {
+			final String commandName = configEntry.getKey();
+			final FileConfiguration config = configEntry.getValue();
+			final String description = config.getString("description", "Opens menu");
+			final String usage = config.getString("usage", "/<command>");
+			final List<String> aliases = config.getStringList("aliases");
+			final List<String> actions = config.getStringList("actions");
+
+			if (commandMap.getCommand(commandName) != null) {
+				Main.getPlugin().getLogger().warning("Skipped registering command /" + commandName + ", it already exists.");
+				continue;
+			}
+
+			commandMap.register("ssx-custom", new Command(commandName, description, usage, aliases) {
+
+				@Override
+				public boolean execute(final CommandSender sender, final String label, final String[] args) {
+					if (sender instanceof Player){
+						final Player player = (Player) sender;
+
+						if (config.isInt("cooldown")) {
+							final long timeLeft = Cooldown.getCooldown("ssxcommand" + commandName);
+							if (timeLeft > 0) {
+								player.sendMessage(String.format(Main.getConfigurationManager().misc.getString("cooldown-message"), timeLeft / 1000.0));
+								return true;
+							}
+
+							Cooldown.addCooldown("ssxcommand" + commandName, config.getInt("cooldown"));
+						}
+
+						Action.runActions(player, actions);
+					}
+					return true;
+				}
+			});
+		}
+
 	}
 
 }
