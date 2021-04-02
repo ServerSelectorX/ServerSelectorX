b7b22b314ae99f92676a204021e88217503624b8
Robin <robinslot@hotmail.nl>
2021-04-02 19:33:07 +0200

    fix imports

--- src/xyz/derkades/serverselectorx/ItemClickListener.java
+++ src/xyz/derkades/serverselectorx/ItemClickListener.java
@@ -8,12 +8,14 @@ import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
+import org.bukkit.event.Listener;
+import org.bukkit.event.block.Action;
 import org.bukkit.event.player.PlayerInteractEvent;
 import org.bukkit.inventory.ItemStack;
 
 import de.tr7zw.changeme.nbtapi.NBTItem;
-import jdk.internal.org.jline.utils.Colors;
 import xyz.derkades.derkutils.Cooldown;
+import xyz.derkades.derkutils.bukkit.Colors;
 
 public class ItemClickListener implements Listener {
 
