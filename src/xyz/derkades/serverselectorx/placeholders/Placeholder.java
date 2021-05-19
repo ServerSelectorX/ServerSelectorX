383cef87a5de3a6530e78deacd7cba135bbf6a8f
Robin <robinslot@hotmail.nl>
2021-05-19 17:43:05 +0200

    more null checks

--- src/xyz/derkades/serverselectorx/placeholders/Placeholder.java
+++ src/xyz/derkades/serverselectorx/placeholders/Placeholder.java
@@ -1,11 +1,13 @@
 package xyz.derkades.serverselectorx.placeholders;
 
+import java.util.Objects;
+
 public abstract class Placeholder {
 
 	private final String key;
 
 	public Placeholder(final String key) {
-		this.key = key;
+		this.key = Objects.requireNonNull(key, "Key is null");
 	}
 
 	public String getKey() {
