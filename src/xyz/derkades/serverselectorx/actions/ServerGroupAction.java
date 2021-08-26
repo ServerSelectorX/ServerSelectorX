52d0c9c5e33f77cfd5eb7bc02f40bcd545e53f01
Robin <robinslot@hotmail.nl>
2021-08-26 11:59:41 +0200

    Add servergroup action for upcoming server group system

---
+++ src/xyz/derkades/serverselectorx/actions/ServerGroupAction.java
@@ -0,0 +1,27 @@
+package xyz.derkades.serverselectorx.actions;
+
+import org.bukkit.entity.Player;
+
+import com.google.common.io.ByteArrayDataOutput;
+import com.google.common.io.ByteStreams;
+
+import xyz.derkades.serverselectorx.Main;
+
+public class ServerGroupAction extends Action {
+
+	private static final String CHANNEL_NAME = "SSX-ServerGroup";
+
+	public ServerGroupAction() {
+		super("servergroup", true);
+	}
+
+	@Override
+	public boolean apply(final Player player, final String value) {
+		final ByteArrayDataOutput out = ByteStreams.newDataOutput();
+		out.writeUTF("Connect");
+		out.writeUTF(value);
+		player.sendPluginMessage(Main.getPlugin(), CHANNEL_NAME, out.toByteArray());
+		return false;
+	}
+
+}
