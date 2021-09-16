3eb0022cac2a2f8ee153e1269ead4215d23d506a
Robin <robin@derkad.es>
2021-09-16 10:04:26 +0200

    unused import

--- src/xyz/derkades/serverselectorx/Main.java
+++ src/xyz/derkades/serverselectorx/Main.java
@@ -10,7 +10,6 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.UUID;
-import java.util.stream.Collectors;
 
 import org.bukkit.Bukkit;
 import org.bukkit.Material;
