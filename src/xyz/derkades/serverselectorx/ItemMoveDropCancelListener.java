601123910452f9e3f9490d6c5c071082260af714
Robin <robinslot@hotmail.nl>
2022-06-02 15:07:00 +0200

    Add option to remove SSX items on death

--- src/xyz/derkades/serverselectorx/ItemMoveDropCancelListener.java
+++ src/xyz/derkades/serverselectorx/ItemMoveDropCancelListener.java
@@ -4,10 +4,10 @@ import de.tr7zw.changeme.nbtapi.NBTItem;
 import org.bukkit.Bukkit;
 import org.bukkit.Material;
 import org.bukkit.World;
-import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
+import org.bukkit.event.entity.PlayerDeathEvent;
 import org.bukkit.event.inventory.ClickType;
 import org.bukkit.event.inventory.InventoryClickEvent;
 import org.bukkit.event.inventory.InventoryDragEvent;
@@ -20,60 +20,7 @@ import java.util.List;
 public class ItemMoveDropCancelListener implements Listener {
 
 	public ItemMoveDropCancelListener() {
-		final FileConfiguration inventory = Main.getConfigurationManager().getInventoryConfiguration();
-
-		if (inventory.getBoolean("cancel-item-drop", false)) {
-			Bukkit.getPluginManager().registerEvents(new Listener() {
-				@EventHandler(ignoreCancelled = true, priority = EventPriority.LOW)
-				public void onDrop(final PlayerDropItemEvent event) {
-					if (!event.getPlayer().hasPermission("ssx.drop") &&
-							isCancelledWorld(event.getPlayer().getWorld()) &&
-							isSsxItem(event.getItemDrop().getItemStack())
-					) {
-						event.setCancelled(true);
-						event.getItemDrop().remove();
-					}
-				}
-			}, Main.getPlugin());
-		}
-
-		if (inventory.getBoolean("cancel-item-move", false)) {
-			Bukkit.getPluginManager().registerEvents(new Listener() {
-				@EventHandler(ignoreCancelled = true, priority = EventPriority.LOW)
-				public void onItemMove(final InventoryClickEvent event) {
-					event.setCancelled(
-						event.getClickedInventory() != null &&
-						!event.getWhoClicked().hasPermission("ssx.move") &&
-						isCancelledWorld(event.getWhoClicked().getWorld()) &&
-						(
-							isSsxItem(event.getCursor()) ||
-							isSsxItem(event.getCurrentItem()) ||
-							(
-								event.getClick() == ClickType.NUMBER_KEY &&
-								isSsxItem(event.getWhoClicked().getInventory().getItem(event.getHotbarButton()))
-							)
-						)
-					);
-				}
-
-				@EventHandler(ignoreCancelled = true, priority = EventPriority.LOW)
-				public void onItemMove(final InventoryDragEvent event){
-					event.setCancelled(
-							!event.getWhoClicked().hasPermission("ssx.move") &&
-							isCancelledWorld(event.getWhoClicked().getWorld()) &&
-							isSsxItem(event.getCursor()));
-				}
-
-				@EventHandler(ignoreCancelled = true, priority = EventPriority.LOW)
-				public void onItemMove(final PlayerSwapHandItemsEvent event) {
-					event.setCancelled(
-							!event.getPlayer().hasPermission("ssx.move") &&
-							isCancelledWorld(event.getPlayer().getWorld()) &&
-							(isSsxItem(event.getMainHandItem())
-									|| isSsxItem(event.getOffHandItem())));
-				}
-			}, Main.getPlugin());
-		}
+		Bukkit.getPluginManager().registerEvents(this, Main.getPlugin());
 	}
 
 	private boolean isCancelledWorld(final World world) {
@@ -83,15 +30,78 @@ public class ItemMoveDropCancelListener implements Listener {
 	}
 
 	private boolean isSsxItem(final ItemStack item) {
-		if (!Main.getConfigurationManager().getInventoryConfiguration().getBoolean("ssx-items-only", false)) {
-			return true;
-		}
-
 		if (item == null || item.getType() == Material.AIR) {
 			return false;
 		} else {
 			final NBTItem nbt = new NBTItem(item);
 			return nbt.hasKey("SSXItem");
+		}
+	}
+
+	@EventHandler(ignoreCancelled = true, priority = EventPriority.LOW)
+	public void onDrop(final PlayerDropItemEvent event) {
+		if (!Main.getConfigurationManager().getInventoryConfiguration().getBoolean("cancel-item-drop")) {
+			return;
+		}
+
+		if (!event.getPlayer().hasPermission("ssx.drop") &&
+				isCancelledWorld(event.getPlayer().getWorld()) &&
+				(
+						!Main.getConfigurationManager().getInventoryConfiguration().getBoolean("ssx-items-only") ||
+						isSsxItem(event.getItemDrop().getItemStack())
+				)
+		) {
+			event.setCancelled(true);
+			event.getItemDrop().remove();
+		}
+	}
+
+	@EventHandler(ignoreCancelled = true, priority = EventPriority.LOW)
+	public void onItemMove(final InventoryClickEvent event) {
+		event.setCancelled(
+				Main.getConfigurationManager().getInventoryConfiguration().getBoolean("cancel-item-move") &&
+				event.getClickedInventory() != null &&
+				!event.getWhoClicked().hasPermission("ssx.move") &&
+				isCancelledWorld(event.getWhoClicked().getWorld()) &&
+				(
+						!Main.getConfigurationManager().getInventoryConfiguration().getBoolean("ssx-items-only") ||
+						isSsxItem(event.getCursor()) ||
+						isSsxItem(event.getCurrentItem()) ||
+						(
+								event.getClick() == ClickType.NUMBER_KEY &&
+								isSsxItem(event.getWhoClicked().getInventory().getItem(event.getHotbarButton()))
+						)
+				)
+		);
+	}
+
+	@EventHandler(ignoreCancelled = true, priority = EventPriority.LOW)
+	public void onItemMove(final InventoryDragEvent event) {
+		event.setCancelled(
+				Main.getConfigurationManager().getInventoryConfiguration().getBoolean("cancel-item-move") &&
+				!event.getWhoClicked().hasPermission("ssx.move") &&
+				isCancelledWorld(event.getWhoClicked().getWorld()) &&
+				isSsxItem(event.getCursor())
+		);
+	}
+
+	@EventHandler(ignoreCancelled = true, priority = EventPriority.LOW)
+	public void onItemMove(final PlayerSwapHandItemsEvent event) {
+		event.setCancelled(
+				Main.getConfigurationManager().getInventoryConfiguration().getBoolean("cancel-item-move") &&
+				!event.getPlayer().hasPermission("ssx.move") &&
+				isCancelledWorld(event.getPlayer().getWorld()) &&
+				(
+						isSsxItem(event.getMainHandItem()) ||
+						isSsxItem(event.getOffHandItem())
+				)
+		);
+	}
+
+	@EventHandler(ignoreCancelled = true, priority = EventPriority.HIGH)
+	public void onDeath(final PlayerDeathEvent event) {
+		if (Main.getConfigurationManager().getInventoryConfiguration().getBoolean("remove-ssx-items-on-death")) {
+			event.getDrops().removeIf(this::isSsxItem);
 		}
 	}
 
