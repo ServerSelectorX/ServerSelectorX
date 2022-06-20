eff5a9a622d308c6bf74e622624a0bc4c2d9bfde
Robin <robinslot@hotmail.nl>
2022-06-20 19:59:44 +0200

    Add debugging to click event

--- src/xyz/derkades/serverselectorx/ItemClickListener.java
+++ src/xyz/derkades/serverselectorx/ItemClickListener.java
@@ -16,22 +16,38 @@ import xyz.derkades.derkutils.Cooldown;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.logging.Logger;
 
 public class ItemClickListener implements Listener {
 
 	@EventHandler(priority = EventPriority.HIGH)
-	public void onInteract(final PlayerInteractEvent event){
+	public void onInteract(final PlayerInteractEvent event) {
+		Logger logger = Main.getPlugin().getLogger();
+
+		if (Main.ITEM_DEBUG) {
+			logger.info("[Click debug] Player " + event.getPlayer().getName() + " performed action " + event.getAction() + " on item using " + event.getHand());
+		}
+
 		if (event.getAction() == Action.PHYSICAL) {
+			if (Main.ITEM_DEBUG) {
+				logger.info("[Click debug] Event was ignored because it was not a click event");
+			}
 			return;
 		}
 
 		if (event.getHand() == EquipmentSlot.OFF_HAND) {
+			if (Main.ITEM_DEBUG) {
+				logger.info("[Click debug] Event was ignored because it was caused by the off hand");
+			}
 			return;
 		}
 
 		final FileConfiguration inventory = Main.getConfigurationManager().getInventoryConfiguration();
 
 		if (event.isCancelled() && inventory.getBoolean("ignore-cancelled", false)) {
+			if (Main.ITEM_DEBUG) {
+				logger.info("[Click debug] Event was ignored because it was cancelled and ignore-cancelled is enabled");
+			}
 			return;
 		}
 
@@ -39,6 +55,9 @@ public class ItemClickListener implements Listener {
 		final ItemStack item = player.getInventory().getItemInMainHand();
 
 		if (item.getType() == Material.AIR) {
+			if (Main.ITEM_DEBUG) {
+				logger.info("[Click debug] Event was ignored because the item was AIR");
+			}
 			return;
 		}
 
@@ -47,12 +66,18 @@ public class ItemClickListener implements Listener {
 		// If the player does not have an open inventory, getOpenInventory returns their crafting or creative inventory
 		if (player.getOpenInventory().getType() != InventoryType.CRAFTING &&
 				player.getOpenInventory().getType() != InventoryType.CREATIVE) {
+			if (Main.ITEM_DEBUG) {
+				logger.info("[Click debug] Event was ignored because the player had an open inventory: " + player.getOpenInventory().getType());
+			}
 			return;
 		}
 
 		final NBTItem nbt = new NBTItem(item);
 
 		if (!nbt.hasKey("SSXActions")) {
+			if (Main.ITEM_DEBUG) {
+				logger.info("[Click debug] Event was ignored because the clicked item is not an SSX item");
+			}
 			// Not an SSX item
 			return;
 		}
@@ -60,6 +85,9 @@ public class ItemClickListener implements Listener {
 		// this cooldown is added when the menu closes
 		String globalCooldownId = player.getName() + "ssxitemglobal";
 		if (Cooldown.getCooldown(globalCooldownId) > 0) {
+			if (Main.ITEM_DEBUG) {
+				logger.info("[Click debug] Event was ignored because the global cooldown was in effect (this cooldown cannot be disabled)");
+			}
 			return;
 		}
 		Cooldown.addCooldown(globalCooldownId, 100);
@@ -73,6 +101,9 @@ public class ItemClickListener implements Listener {
 		}
 
 		if (actions.isEmpty()) {
+			if (Main.ITEM_DEBUG) {
+				logger.info("[Click debug] Event was ignored because the item has no actions");
+			}
 			return;
 		}
 
@@ -81,6 +112,9 @@ public class ItemClickListener implements Listener {
 			if (Cooldown.getCooldown(cooldownId) > 0) {
 				List<String> cooldownActions = nbt.getStringList("SSXCooldownActions");
 				xyz.derkades.serverselectorx.actions.Action.runActions(player, cooldownActions);
+				if (Main.ITEM_DEBUG) {
+					logger.info("[Click debug] Event was ignored because the configured cooldown was in effect");
+				}
 				return;
 			}
 			int cooldownTime = nbt.getInteger("SSXCooldownTime");
@@ -89,8 +123,15 @@ public class ItemClickListener implements Listener {
 
 		xyz.derkades.serverselectorx.actions.Action.runActions(player, actions);
 
+		if (Main.ITEM_DEBUG) {
+			logger.info("[Click debug] Event handling completed, actions have been run.");
+		}
+
 		final boolean cancel = inventory.getBoolean("cancel-click-event", false);
 		if (cancel) {
+			if (Main.ITEM_DEBUG) {
+				logger.info("[Click debug] The event has been cancelled, because cancel-click-event is enabled");
+			}
 			event.setCancelled(true);
 		}
 	}
