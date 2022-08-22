b9f4f3b737df195e38fd658d4efa1dc17c4e19c0
Robin <robinslot@hotmail.nl>
2022-08-22 17:37:05 +0200

    Register open-menu condition

--- src/xyz/derkades/serverselectorx/conditional/condition/Conditions.java
+++ src/xyz/derkades/serverselectorx/conditional/condition/Conditions.java
@@ -14,6 +14,7 @@ public class Conditions {
 		Condition[] conditions = new Condition[] {
 				new CurrentServerCondition(),
 				new EffectCondition(),
+				new OpenMenuCondition(),
 				new PapiCondition(),
 				new PermissionCondition(),
 				new ServerPlaceholderCondition(),
