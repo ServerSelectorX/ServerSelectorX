079e8ed7737570cc875ad051706a4c82d9a854f0
Robin <robin+git@rkslot.nl>
2022-11-10 09:39:35 +0100

    Add 'in-world' condition

---
+++ src/xyz/derkades/serverselectorx/conditional/condition/InWorldCondition.java
@@ -0,0 +1,36 @@
+package xyz.derkades.serverselectorx.conditional.condition;
+
+import org.bukkit.configuration.InvalidConfigurationException;
+import org.bukkit.entity.Player;
+
+import java.util.List;
+import java.util.Map;
+
+public class InWorldCondition extends Condition {
+
+    InWorldCondition() {
+        super("in-world");
+    }
+
+    @Override
+    public boolean isTrue(Player player, Map<String, Object> options) throws InvalidConfigurationException {
+        if (options.containsKey("world")) {
+            if (options.containsKey("worlds")) {
+                throw new InvalidConfigurationException("Cannot use 'world' and 'worlds' option at the same time");
+            }
+
+            String worldName = (String) options.get("world");
+            return player.getWorld().getName().equals(worldName);
+        } else if (options.containsKey("worlds")) {
+            List<String> worldNames = (List<String>) options.get("worlds");
+            for (String worldName : worldNames) {
+                if (player.getWorld().getName().equals(worldName)) {
+                    return true;
+                }
+            }
+            return false;
+        } else {
+            throw new InvalidConfigurationException("Missing required 'worlds' option (list of world names) or 'world' option (single world name)");
+        }
+    }
+}
